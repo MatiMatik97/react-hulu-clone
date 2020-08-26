0f594fc277012276f5b50ecbbb8d85fa3a063fa3
Mateusz <matimatik97@gmail.com>
2020-08-26 14:43:09 +0200

    DEV-002 - Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
 // 7bf87ba6b764dccc3b888fcf1277b239
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="app">
       <Header />
+      <Nav />
     </div>
   );
 };
