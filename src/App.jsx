b1edd653aea3e300a00c5ff795ddea385f2072d4
Mateusz <matimatik97@gmail.com>
2020-08-26 14:31:46 +0200

    DEV-001 - Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+
+// 7bf87ba6b764dccc3b888fcf1277b239
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hulu!</h1>
+      <Header />
     </div>
   );
 };
